7bde0f31994f1b44074fe4bdc3828b0a13894608
pablomuro <pablo.muro@gmail.com>
2017-04-20 01:03:31 +0000

    Task 10 com função run trocadad por loop for

--- test-ball.cpp
+++ test-ball.cpp
@@ -5,6 +5,10 @@
 
 #include "ball.h"
 
+void run(Simulation & s, double dt) {
+  for (int i = 0 ; i < 100 ; ++i) { s.step(dt) ; s.display() ; }
+}
+
 int main(int argc, char** argv)
 {
   //Inicialização com parãmetros e SET de novas posições
@@ -12,9 +16,13 @@ int main(int argc, char** argv)
   ball.setPositionX(0.7);
   ball.setPositionY(0.7);
   const double dt = 1.0/30 ;
-  for (int i = 0 ; i < 100 ; ++i) {
+/*  for (int i = 0 ; i < 100 ; ++i) {
     ball.step(dt) ;
     ball.display() ;
   }
+  */
+  
+  run(ball, dt);
+  
   return 0 ;
 }
